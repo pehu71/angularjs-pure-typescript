dabe850a1c5c3909b8f5bcbb48f0e83f34ea8ee2
pehu71 <humplikpetr@gmail.com>
2017-01-24 20:02:14 +0100

    systemjs module loader

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,27 +1,26 @@
 (function (global) {
     System.config({
 
-        /*        paths: {
-         // paths serve as alias
-         'npm:': 'script/'
-         },*/
+        paths: {
+            // paths serve as alias
+            'npm:': 'node_modules/'
+        },
         // map tells the System loader where to look for things
         map: {
             // our app is within the app folder
             app: 'app',
-            angular: 'node_modules/angular'
-        }
-        ,
+            angular: 'npm:angular'
+        },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
             app: {
                 main: 'app.js',
                 defaultExtension: 'js'
             },
-             angular: {
-                 main: 'index.js',
-                 defaultExtension: 'js'
-             }
+            angular: {
+                main: 'index.js',
+                defaultExtension: 'js'
+            }
         }
     });
 })(this);
