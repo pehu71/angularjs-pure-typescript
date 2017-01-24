d08ffb7c4fc6e75936a0bb6f5e066fe22f46c29d
pehu71 <humplikpetr@gmail.com>
2017-01-24 21:25:37 +0100

    model to view

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,8 @@
 import * as angular from "angular"
 import {IndexCtrl} from "./index-ctrl";
 
-
 angular.module('tsApp', [])
+
     .controller({'IndexCtrl': () => new IndexCtrl()});
+
 angular.bootstrap(document, ['tsApp']);
