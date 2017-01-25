8f3f0354cc16c36feb692090e651c53e23bdb96a
pehu71 <humplikpetr@gmail.com>
2017-01-25 12:37:04 +0100

    routing on booksCmp

---
+++ app/books-cmp.js
@@ -0,0 +1,9 @@
+"use strict";
+var booksCmp = (function () {
+    function booksCmp() {
+        this.templateUrl = 'view/books.html';
+    }
+    return booksCmp;
+}());
+exports.booksCmp = booksCmp;
+//# sourceMappingURL=books-cmp.js.map
