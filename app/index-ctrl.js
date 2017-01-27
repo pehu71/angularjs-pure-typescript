efdc2150687a72604b0803d0fde4bd3246d28de3
Petr -pehu- Humplik <humplikpetr@gmail.com>
2017-01-27 09:52:19 +0100

    Delete index-ctrl.js

--- app/index-ctrl.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var IndexCtrl = (function () {
-    function IndexCtrl() {
-        this.pageTitle = 'Home';
-    }
-    return IndexCtrl;
-}());
-exports.IndexCtrl = IndexCtrl;
-//# sourceMappingURL=index-ctrl.js.map
